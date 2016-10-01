d3d2ac48bb9492212a15e52cc2e61ad9c2520c47
David Mattes <mattes@nixnux.org>
2016-09-30 22:34:46 -0700

    CON-49: Improve jsrunner output to include payload headers and correct number of violations.

--- services/config.rb
+++ services/config.rb
@@ -36,37 +36,60 @@ end
 # to have at least one region that logs global services events.
 coreo_uni_util_jsrunner "cloudtrail-aggregate" do
   action :run
-  json_input 'STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.report'
+  json_input '{"stack name":"INSTANCE::stack_name",
+  "instance name":"INSTANCE::name",
+  "number_of_checks":"STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_checks",
+  "number_of_violations":"STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_violations",
+  "number_violations_ignored":"STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_ignored_violations",
+  "violations":"STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.report"'
   function <<-EOH
 var result = {};
-console.log(util.inspect(json_input, {showHidden: false, depth: null}));
+result['violations'] = {};
+result['stack name'] = json_input['stack name'];
+result['instance name'] = json_input['instance name'];
+result['number_of_checks'] = json_input['number_of_checks'];
+result['number_violations_ignored'] = json_input['number_violations_ignored'];
 
 var nRegionsWithGlobal = 0;
-for (var key in json_input) {
+var nViolations = 0;
+
+console.log('json_input: ' + JSON.stringify(json_input));
+
+for (var key in json_input['violations') {
   if (json_input.hasOwnProperty(key)) {
     console.log('checking key: ' + key);
     if (json_input[key]['violations']['trail-with-global']) {
       console.log("Trail has a region with global: " + key);
       nRegionsWithGlobal++;
     } else {
-      result[key] = json_input[key];
+      nViolations++;
+      result['violations'][key] = json_input[key];
     }
   }
 }
+
 console.log('Number of regions with global: ' + nRegionsWithGlobal);
 
 var noGlobalsAlert = {};
 if (nRegionsWithGlobal == 0) {
-  noGlobalsAlert = { violations:
+  nViolations++;
+  noGlobalsAlert = 
+          { violations:
             { 'no-global-trails':
                { description: 'CloudTrail global service logging is not enabled for the selected regions.',
                  category: 'Audit',
                  suggested_action: 'Enable CloudTrail global service logging in at least one region',
                  level: 'Warning',
-                 region: 'selected-regions' } },
-           tags: [] };
-  result['selected-regions'] = noGlobalsAlert;
+                 region: 'selected-regions'
+               }
+            },
+            tags: []
+          };
+  result['violations']['selected-regions'] = noGlobalsAlert;
 }
+
+console.log('Number of violations: ' + nViolations);
+result['number_of_violations'] = nViolations;
 
 callback(result);
 EOH
@@ -77,12 +100,7 @@ coreo_uni_util_notify "advise-cloudtrail" do
   type 'email'
   allow_empty ${AUDIT_AWS_CLOUDTRAIL_ALLOW_EMPTY}
   send_on "${AUDIT_AWS_CLOUDTRAIL_SEND_ON}"
-  payload '{"stack name":"INSTANCE::stack_name",
-  "instance name":"INSTANCE::name",
-  "number_of_checks":"STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_checks",
-  "number_of_violations":"STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_violations",
-  "number_violations_ignored":"STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_ignored_violations",
-  "violations": STACK::coreo_uni_util_jsrunner.cloudtrail-aggregate.return }'
+  payload 'STACK::coreo_uni_util_jsrunner.cloudtrail-aggregate.return'
   payload_type "${AUDIT_AWS_CLOUDTRAIL_PAYLOAD_TYPE}"
   endpoint ({ 
               :to => '${AUDIT_AWS_CLOUDTRAIL_ALERT_RECIPIENT}', :subject => 'CloudCoreo cloudtrail advisor alerts on INSTANCE::stack_name :: INSTANCE::name'
