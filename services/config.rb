891e8b93da64d066239e0cee14c0bd1710748173
David Mattes <mattes@nixnux.org>
2016-10-04 11:52:00 -0700

    CON-49: back to original use of config.yaml, with engine change on var substitution output.

--- services/config.rb
+++ services/config.rb
@@ -36,13 +36,13 @@ end
 # to have at least one region that logs global services events.
 coreo_uni_util_jsrunner "cloudtrail-aggregate" do
   action :run
-  json_input "{'stack name':'INSTANCE::stack_name',
-  'instance name':'INSTANCE::name',
-  'regions':'selected',
-  'number_of_checks':'STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_checks',
-  'number_of_violations':'STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_violations',
-  'number_violations_ignored':'STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_ignored_violations',
-  'violations':STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.report}"
+  json_input '{"stack name":"INSTANCE::stack_name",
+  "instance name":"INSTANCE::name",
+  "regions":${AUDIT_AWS_CLOUDTRAIL_REGIONS},
+  "number_of_checks":"STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_checks",
+  "number_of_violations":"STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_violations",
+  "number_violations_ignored":"STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_ignored_violations",
+  "violations":STACK::coreo_aws_advisor_cloudtrail.advise-cloudtrail.report}'
   function <<-EOH
 var result = {};
 result['violations'] = {};
