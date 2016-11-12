1c3e81e66b2be3a6d80c8997b4606e5ea48038fe
George Moberly <georgemoberly@Georges-MacBook-Air-2.local>
2016-11-12 05:35:13 -0800

    testing

--- services/config.rb
+++ services/config.rb
@@ -40,7 +40,6 @@ coreo_uni_util_jsrunner "cloudtrail-aggregate" do
   action :run
   json_input '{"stack name":"PLAN::stack_name",
   "instance name":"PLAN::name",
-  //"regions":"${AUDIT_AWS_CLOUDTRAIL_REGIONS}",
   "number_of_checks":"COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_checks",
   "number_of_violations":"COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_violations",
   "number_violations_ignored":"COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_ignored_violations",
@@ -116,14 +115,14 @@ end
 
 # this is the new notifier that takes the violations transformed by the jsrunner
 #
-# coreo_uni_util_notify "advise-cloudtrail" do
-#   action :notify
-#   type 'email'
-#   allow_empty ${AUDIT_AWS_CLOUDTRAIL_ALLOW_EMPTY}
-#   send_on "${AUDIT_AWS_CLOUDTRAIL_SEND_ON}"
-#   payload 'COMPOSITE::coreo_uni_util_jsrunner.cloudtrail-aggregate.return'
-#   payload_type "json"
-#   endpoint ({ 
-#               :to => '${AUDIT_AWS_CLOUDTRAIL_ALERT_RECIPIENT}', :subject => 'CloudCoreo cloudtrail advisor alerts on PLAN::stack_name :: PLAN::name'
-#             })
-# end
+coreo_uni_util_notify "advise-cloudtrail" do
+  action :notify
+  type 'email'
+  allow_empty ${AUDIT_AWS_CLOUDTRAIL_ALLOW_EMPTY}
+  send_on "${AUDIT_AWS_CLOUDTRAIL_SEND_ON}"
+  payload 'COMPOSITE::coreo_uni_util_jsrunner.cloudtrail-aggregate.return'
+  payload_type "json"
+  endpoint ({ 
+              :to => '${AUDIT_AWS_CLOUDTRAIL_ALERT_RECIPIENT}', :subject => 'CloudCoreo cloudtrail advisor alerts on PLAN::stack_name :: PLAN::name'
+            })
+end
