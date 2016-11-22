8c553c6fa0c8f34cfec51ea23c4f7592c1e87c5a
123prove123 <123prove123@gmail.com>
2016-11-22 17:06:48 +0600

    CON-115 - refactor code and change version

--- services/config.rb
+++ services/config.rb
@@ -78,10 +78,13 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array" do
   packages([
         {
           :name => "cloudcoreo-jsrunner-commons",
-          :version => "1.0.0"
+          :version => "1.0.2"
         }       ])
-  json_input '{"stack_name":"PLAN::stack_name",
-                "instance_name":"PLAN::name",
+  json_input '{"stack name":"PLAN::stack_name",
+                "instance name":"PLAN::name",
+                "number_of_checks":"COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_checks",
+                "number_of_violations":"COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_violations",
+                "number_violations_ignored":"COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_ignored_violations"
                 "violations": COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.report}'
   function <<-EOH
 const CloudCoreoJSRunner = require('cloudcoreo-jsrunner-commons');
