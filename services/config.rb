f5a9dac60e1d5a6fd68227a9fec229543e488cc6
123prove123 <123prove123@gmail.com>
2016-11-23 09:16:28 +0600

    CON-115 - fixed bag with json 3

--- services/config.rb
+++ services/config.rb
@@ -82,9 +82,6 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array" do
         }       ])
   json_input '{"stack name":PLAN::stack_name,
                 "instance name":PLAN::name,
-                "number_of_checks":COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_checks,
-                "number_of_violations":COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_violations,
-                "number_violations_ignored":COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_ignored_violations,
                 "violations": COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.report}'
   function <<-EOH
 const CloudCoreoJSRunner = require('cloudcoreo-jsrunner-commons');
