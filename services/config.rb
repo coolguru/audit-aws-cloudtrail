44f828b8c27f4b71a6542e7b7430110961e2da94
123prove123 <123prove123@gmail.com>
2016-11-30 11:39:06 +0600

    test 1

--- services/config.rb
+++ services/config.rb
@@ -36,6 +36,76 @@ coreo_aws_advisor_cloudtrail "advise-cloudtrail" do
 end
 
 
+coreo_uni_util_jsrunner "cloudtrail-aggregate" do
+  action :run
+  json_input '{"stack name":"PLAN::stack_name",
+  "instance name":"PLAN::name",
+  "number_of_checks":"COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_checks",
+  "number_of_violations":"COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_violations",
+  "number_violations_ignored":"COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.number_ignored_violations",
+  "violations":COMPOSITE::coreo_aws_advisor_cloudtrail.advise-cloudtrail.report}'
+  function <<-EOH
+var_regions = "${AUDIT_AWS_CLOUDTRAIL_REGIONS}";
+var result = {};
+result['stack name'] = json_input['stack name'];
+result['instance name'] = json_input['instance name'];
+result['regions'] = var_regions;
+result['number_of_checks'] = json_input['number_of_checks'];
+result['number_violations_ignored'] = json_input['number_violations_ignored'];
+result['violations'] = {};
+var nRegionsWithGlobal = 0;
+var nViolations = 0;
+console.log('json_input: ' + JSON.stringify(json_input));
+for (var key in json_input['violations']) {
+  if (json_input['violations'].hasOwnProperty(key)) {
+    console.log('--> checking key: ' + key);
+    if (json_input['violations'][key]['violations']['cloudtrail-trail-with-global']) {
+      console.log("Trail has a region with global: " + key);
+      nRegionsWithGlobal++;
+    } else {
+      nViolations++;
+      result['violations'][key] = json_input['violations'][key];
+    }
+  }
+}
+console.log('Number of regions with global: ' + nRegionsWithGlobal);
+var noGlobalsAlert = {};
+if (nRegionsWithGlobal == 0) {
+  nViolations++;
+  noGlobalsAlert =
+          { violations:
+            { 'no-global-trails':
+               { 'description': 'CloudTrail global service logging is not enabled for the selected regions.',
+                 'category': 'Audit',
+                 'suggested_action': 'Enable CloudTrail global service logging in at least one region',
+                 'level': 'Warning',
+                 'regions': var_regions
+               }
+            },
+            tags: []
+          };
+  var key = 'selected regions';
+  console.log('saving global violation on key: ' + key + ' | violation: ' + JSON.stringify(noGlobalsAlert));
+  result['violations']['selected-regions'] = noGlobalsAlert;
+}
+console.log('Number of violations: ' + nViolations);
+result['number_of_violations'] = nViolations;
+callback(result);
+  EOH
+end
+
+coreo_uni_util_notify "advise-cloudtrail-2" do
+  action :notify
+  type 'email'
+  allow_empty ${AUDIT_AWS_CLOUDTRAIL_ALLOW_EMPTY}
+  send_on "${AUDIT_AWS_CLOUDTRAIL_SEND_ON}"
+  payload 'COMPOSITE::coreo_uni_util_jsrunner.cloudtrail-aggregate.return'
+  payload_type "json"
+  endpoint ({
+      :to => '${AUDIT_AWS_CLOUDTRAIL_ALERT_RECIPIENT}', :subject => 'CloudCoreo cloudtrail advisor alerts on PLAN::stack_name :: PLAN::name'
+  })
+end
+
 coreo_uni_util_notify "advise-cloudtrail-json" do
   action :${AUDIT_AWS_CLOUDTRAIL_FULL_JSON_REPORT}
   type 'email'
