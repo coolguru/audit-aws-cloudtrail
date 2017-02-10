638dcbc659e449a9a980f247c4b7434a6d4a65ac
Michael Pautov <123prove123@gmail.com>
2017-02-10 13:34:09 +0600

    add table yaml

--- services/config.rb
+++ services/config.rb
@@ -220,7 +220,7 @@ function setTableAndSuppression() {
   const yaml = require('js-yaml');
   try {
       table = yaml.safeLoad(fs.readFileSync('./table.yaml', 'utf8'));
-      suppression = yaml.safeLoad(fs.readFileSync('./suppression.yaml', 'utf8'));
+      suppression = yaml.safeLoad(fs.readFileSync('./table.yaml', 'utf8'));
   } catch (e) {
   }
   coreoExport('table', JSON.stringify(table));
