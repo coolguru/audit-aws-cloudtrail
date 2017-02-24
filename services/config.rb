7bcc7670835a4100b130800cd1aed9c10e5c0112
colemai <colemai@tcd.ie>
2017-02-24 09:26:11 -0800

    CON-355 variation

--- services/config.rb
+++ services/config.rb
@@ -70,7 +70,7 @@ coreo_aws_rule "cloudtrail-logs-encrypted" do
   meta_cis_scored "true"
   meta_cis_level "2"
   objectives ["trails"]
-  audit_objects ["object.trail_list".to_s]
+  audit_objects ["trail_list"]
   operators ["!~"]
   raise_when [/kms/]
   id_map "object.trail_list.name"
