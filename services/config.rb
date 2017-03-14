e91401490c34099ff828533f443c0f3a38559983
Michael Pautov <michael.pautov@gmail.com>
2017-03-14 13:28:07 +0600

    Updated CloudCoreo JSrunner version 1.9.4

--- services/config.rb
+++ services/config.rb
@@ -293,7 +293,7 @@ coreo_uni_util_jsrunner "cloudtrail-tags-to-notifiers-array" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.9.3"
+                   :version => "1.9.4"
                },
                {
                    :name => "js-yaml",
