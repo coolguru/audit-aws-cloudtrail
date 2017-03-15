df4c955ef9006bdb0d7ee4219010c8be2bbc106a
Paul Allen <pauldallen@gmail.com>
2017-03-15 14:35:26 -0700

    fixing jsrunner-commons version

--- services/config.rb
+++ services/config.rb
@@ -293,7 +293,7 @@ coreo_uni_util_jsrunner "cloudtrail-tags-to-notifiers-array" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "latest"
+                   :version => "1.9.2"
                },
                {
                    :name => "js-yaml",
